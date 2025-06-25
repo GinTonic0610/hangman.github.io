b88dc8028589501129374213be3614b506526815
GinTonic0610 <leone.pasqualucci@gmail.com>
2025-06-25 16:40:27 +0200

    css fix8

--- main.js
+++ main.js
@@ -1,111 +1,115 @@
 const arr = [
-  "Cane",
-  "Gatto",
-  "Pane",
-  "Mela",
-  "Sedia",
-  "Tavolo",
-  "Scuola",
-  "Fiume",
-  "Bosco",
-  "Sole",
-  "Lampada",
-  "Inverno",
-  "Pinguino",
-  "Matita",
-  "Gelato",
-  "Montagna",
-  "Bambino",
-  "Sentiero",
-  "Castello",
-  "Specchio",
-  "Ombrello",
-  "Zucchero",
-  "Sciarpa",
-  "Trampolino",
-  "Fioraio",
-  "Zattera",
-  "Atlante",
-  "Calamaro",
-  "Elefante",
-  "Sottopassaggio",
-  "Gorgoglìo",
-  "Scherzo",
-  "Rizoma",
-  "Schiuma",
-  "Quercia",
-  "Clandestino",
-  "Scombussolato",
-  "Fisioterapia",
-  "Coscienza",
-  "Dissonanza",
-  "Astruso",
-  "Bisbiglio",
-  "Zigzagare",
-  "Starnuto",
-  "Gazzella",
-  "Sbadiglio",
-  "Sguattero",
-  "Taccuino",
-  "Vorticare",
-  "Gnomone",
-  "Inarrestabile",
-  "Sovraccarico",
-  "Incomprensibile",
-  "Sottosopra",
-  "Sovraffollato",
-  "Parallelepipedo",
-  "Involontariamente",
-  "Maldestramente",
-  "Insopportabile",
-  "Sovrastruttura",
-  "Uva",
-  "Eco",
-  "Ape",
-  "Re",
-  "Oca",
-  "Boa",
-  "Ice",
-  "Ufo",
-  "Van",
-  "Top",
-  "Jolly",
-  "Koala",
-  "Taxi",
-  "Yogurt",
-  "Whisky",
-  "Xenofobo",
-  "Jackpot",
-  "Kayak",
-  "Weekend",
-  "Extravergine",
-  "Portaombrelli",
-  "Capolavoro",
-  "Sovraccarico",
-  "Batticuore",
-  "Rompicapo",
-  "Paracadute",
-  "Spazzacamino",
-  "Sottaceto",
-  "Acchiappasogni",
-  "Fischiettare",
-  "Fantasma",
-  "Tenebre",
-  "Incubo",
-  "Segreto",
-  "Brivido",
-  "Sussurro",
-  "Oscurità",
-  "Macabro",
-  "Labirinto",
-  "Stregoneria",
+  "CANE",
+  "GATTO",
+  "PANE",
+  "MELA",
+  "SEDIA",
+  "TAVOLO",
+  "SCUOLA",
+  "FIUME",
+  "BOSCO",
+  "SOLE",
+  "LAMPADA",
+  "INVERNO",
+  "PINGUINO",
+  "MATITA",
+  "GELATO",
+  "MONTAGNA",
+  "BAMBINO",
+  "SENTIERO",
+  "CASTELLO",
+  "SPECCHIO",
+  "OMBRELLO",
+  "ZUCCHERO",
+  "SCIARPA",
+  "TRAMPOLINO",
+  "FIORAIO",
+  "ZATTERA",
+  "ATLANTE",
+  "CALAMARO",
+  "ELEFANTE",
+  "SOTTOPASSAGGIO",
+  "GORGOGLIO",
+  "SCHERZO",
+  "RIZOMA",
+  "SCHIUMA",
+  "QUERCIA",
+  "CLANDESTINO",
+  "SCOMBUSSOLATO",
+  "FISIOTERAPIA",
+  "COSCIENZA",
+  "DISSONANZA",
+  "ASTRUSO",
+  "BISBIGLIO",
+  "ZIGZAGARE",
+  "STARNUTO",
+  "GAZZELLA",
+  "SBADIGLIO",
+  "SGUATTERO",
+  "TACCUINO",
+  "VORTICARE",
+  "GNOMONE",
+  "INARRESTABILE",
+  "SOVRACCARICO",
+  "INCOMPRENSIBILE",
+  "SOTTOSOPRA",
+  "SOVRAFFOLLATO",
+  "PARALLELEPIPEDO",
+  "INVOLONTARIAMENTE",
+  "MALDESTRAMENTE",
+  "INSOPPORTABILE",
+  "SOVRASTRUTTURA",
+  "UVA",
+  "ECO",
+  "APE",
+  "RE",
+  "OCA",
+  "BOA",
+  "ICE",
+  "UFO",
+  "VAN",
+  "TOP",
+  "JOLLY",
+  "KOALA",
+  "TAXI",
+  "YOGURT",
+  "WHISKY",
+  "XENOFOBO",
+  "JACKPOT",
+  "KAYAK",
+  "WEEKEND",
+  "EXTRAVERGINE",
+  "PORTAOMBRELLI",
+  "CAPOLAVORO",
+  "SOVRACCARICO",
+  "BATTICUORE",
+  "ROMPICAPO",
+  "PARACADUTE",
+  "SPAZZACAMINO",
+  "SOTTACETO",
+  "ACCHIAPPASOGNI",
+  "FISCHIETTARE",
+  "FANTASMA",
+  "TENEBRE",
+  "INCUBO",
+  "SEGRETO",
+  "BRIVIDO",
+  "SUSSURRO",
+  "OSCURITA",
+  "MACABRO",
+  "LABIRINTO",
+  "STREGONERIA"
 ];
 
 const div = document.getElementById("app");
 const dialog = document.getElementById("di");
-const cButton = document.getElementById("closeButton");
+const outcomeButton = document.getElementById("closeButton");
+const clearButton = document.getElementById("clearButton");
+const sendButton = document.getElementById("sendButton");
 const outcome = document.getElementById("outcome");
 const input = document.getElementById("log");
+const alert = document.getElementById("alert");
+const keyArea = document.getElementById("letters");
 
 function getWord() {
   const t = arr[Math.floor(Math.random() * arr.length)];
@@ -130,13 +134,13 @@ function renderLives() {
 }
 
 function startGame() {
-    word = getWord().toLocaleLowerCase();
+    word = getWord();
     resetAvailableLetters();
     letters = [];
     nl = 0;
     lives = 5;
     renderLives()
-    document.addEventListener('keydown', onKeydown);
+    setListeners();
 }
 
 function paintLetter(i) {
@@ -146,53 +150,92 @@ function paintLetter(i) {
 
 function livesDecrement() {
   lives--;
-  renderLives()
+  renderLives();
   if (lives === 0) {
-    console.log("You Lost");
-    outcome.textContent = "You Lost";
-    document.removeEventListener('keydown', onKeydown);
-    dialog.showModal();
+    showOutcome("You Lost");
   }
 }
 
-function win() {
-  console.log("You win");
-  outcome.textContent = "You Win";
-  document.removeEventListener('keydown', onKeydown);
+function showOutcome(o) {
+  remouveListeners();
+  outcome.textContent = o;
   dialog.showModal();
 }
 
 function resetAvailableLetters() {
   for (let i = 0; i < letters.length; i++) {
-    const p = document.getElementById(letters[i].toUpperCase());
+    const p = document.getElementById(letters[i]);
     if (p) p.style.color = "black";
   }
 }
 
 function removeFromAvailableLetters(l) {
-  const p = document.getElementById(l.toUpperCase());
+  letters.push(l);
+  const p = document.getElementById(l);
   if (p) p.style.color = "transparent";
 }
 
-function onKeydown(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    event.stopPropagation();
+function sendAllert(message) {
+    alert.textContent = message;
+    setTimeout(() => { alert.textContent = ""; }, 1000);
+}
+
+function onKeydown(e) {
+  e.preventDefault();
+  if (e.key === "Enter") {
     guessWord(input.value);
-  } else if (event.key === "Backspace") {
-    input.value = input.value.slice(0, -1);
-  } else if (event.key.length === 1) {
-    input.value += event.key;
+  } 
+  else if (e.key === "Backspace") {
+    if (e.ctrlKey || e.metaKey) {
+      input.value = ""; 
+    }
+    else { 
+      input.value = input.value.slice(0, -1); 
+    }
   }
+  else if (e.key.length === 1) {
+    if (input.value.length >= word.length) {
+      sendAllert("Over word length !");
+      return;
+    }
+    input.value += e.key.toUpperCase();
+  }
+}
+
+function onClick(e){
+  if (e.target.tagName === 'P') {
+    const letter = e.target.textContent;
+    const ke = new KeyboardEvent('keydown', {
+      key: letter,
+      bubbles: true,
+      cancelable: true
+    });
+    document.dispatchEvent(ke);
+  }
+}
+
+function setListeners() {
+  document.addEventListener('keydown', onKeydown);
+  keyArea.addEventListener('click', onClick);
+}
+function remouveListeners() {
+  keyArea.removeEventListener('click', onClick);
+  document.removeEventListener('keydown', onKeydown);
 }
 
 function guessWord(guess) {
     console.log(guess);
     input.value = "";
 
+    if(guess === "") {
+      sendAllert("Empty guess !");
+      return;
+    }
+
     if (guess.length === 1) {
         if (letters.includes(guess)) {
-            return; // Already guessed this letter
+          sendAllert("Letter already guessed !");
+          return;
         }
         let r = false;
         for (let i = 0; i < word.length; i++) {
@@ -201,12 +244,11 @@ function guessWord(guess) {
                 nl++;
                 r = true;
                 if (nl === word.length) {
-                    win();
-                    return;
+                  showOutcome("You Won");
+                  return;
                 }
             }
         }
-        letters.push(guess);
         removeFromAvailableLetters(guess);
         if (r) {
             return;
@@ -217,20 +259,27 @@ function guessWord(guess) {
         for (let i = 0; i < word.length; i++) {
             paintLetter(i)
         }
-        win();
+        showOutcome("You Won");
         return;
     }
 
     livesDecrement();
 };
 
-cButton.onclick = () => {
+outcomeButton.onclick = () => {
   dialog.close();
-
   while (div.firstChild) {
     div.removeChild(div.lastChild);
   }
   startGame()
+};
+
+clearButton.onclick = () => {
+  input.value = "";
+}
+
+sendButton.onclick = () => {
+  guessWord(input.value);
 };
 
 startGame();
